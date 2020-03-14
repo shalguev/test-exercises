e24d48d26dd9440612126005d899c00f19638650
Oleg Shalguev <48881876+shalguev@users.noreply.github.com>
2020-03-14 14:09:25 +0300

    Create SlowRecursiveFib.java

---
+++ fibonacci/recursive/SlowRecursiveFib.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(fib(50));
+  }
+
+  private static long fib(final long n) {
+    if(n <= 2){
+      return 1L;
+    } else {
+      return fib(n - 1) + fib(n - 2);
+    }
+  }
+}
